f72d56a08c244e5558e92880927632d38229f5c5
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-07-11 22:50:46 +0200

    adding vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import GoBackButton from "@/components/GoBackButton";
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Roboto_Slab } from "next/font/google";
 
 const roboto = Roboto_Slab({ subsets: ["latin"] });
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <link rel='icon' href='/ecus.webp' sizes='any' />
-      <body className={`${roboto.className} bg-blue-900`}>{children}</body>
+      <body className={`${roboto.className} bg-blue-900`}>
+        {children} <Analytics />
+      </body>
     </html>
   );
 }
