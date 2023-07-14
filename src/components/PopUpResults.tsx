ab863de8be7b0bc02dc9c07f0dcc18fcd9eb59bf
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-07-14 19:33:16 +0200

    adding test page

--- src/components/PopUpResults.tsx
+++ src/components/PopUpResults.tsx
@@ -1,3 +1,5 @@
+import { Category } from "@/types";
+
 const PopUpResults: React.FC<{ result: Category }> = ({ result }) => {
   // Render the result popup component with the received result
   return (
