5c79b5fae1d9510f74e0a18fcd2aecb0f26c777f
Cyprien de Fontenay <cbdefontenay@gmail.com>
2023-07-14 22:42:03 +0200

    changes

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -1,316 +1,298 @@
 export const questions = [
   {
-    text: "You are walking through the Forbidden Forest and come across a wounded animal. What do you do?",
+    text: "Which magical creature would you most like to have as a pet?",
     choices: [
       {
-        choice: "Tend to its wounds and try to find help.",
+        choice: "Phoenix",
         category: "Gryffindor",
       },
       {
-        choice: "Leave it alone and continue exploring.",
+        choice: "Owl",
         category: "Ravenclaw",
       },
       {
-        choice: "Capture it for study.",
+        choice: "Mouse",
         category: "Slytherin",
       },
       {
-        choice: "Try to tame it and make it your companion.",
+        choice: "Hippogriff",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "During a Quidditch match, you notice a teammate struggling to keep up. What do you do?",
+    text: "Which Hogwarts subject is the most fascinating to you?",
     choices: [
       {
-        choice: "Encourage and support them from the sidelines",
+        choice: "Defense Against the Dark Arts",
         category: "Gryffindor",
       },
       {
-        choice: "Analyze the game and strategize a better approach",
+        choice: "Potions",
         category: "Ravenclaw",
       },
       {
-        choice: "Take advantage of the distraction to gain an advantage",
+        choice: "Dark Arts",
         category: "Slytherin",
       },
       {
-        choice: "Take their place to help the team secure victory",
+        choice: "Care of Magical Creatures",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "You witness a fellow student cheating on a test. What do you do?",
+    text: "If you could choose any magical object, which one would you pick?",
     choices: [
       {
-        choice: "Confront and convince them to stop cheating",
+        choice: "Invisibility Cloak",
         category: "Gryffindor",
       },
       {
-        choice: "Report the incident to the professor without hesitation",
+        choice: "Time Turner",
         category: "Ravenclaw",
       },
       {
-        choice: "Blackmail them for your own benefit in the future",
+        choice: "Sneakoscope",
         category: "Slytherin",
       },
       {
-        choice: "Offer to help them study and understand the material",
+        choice: "Marauder's Map",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "You come across a locked door with a sign that says 'Do Not Enter.' What do you do?",
+    text: "Wich quote from Dumbledore speaks to you?",
     choices: [
       {
-        choice: "Respect the sign and find an alternative route.",
-        category: "Gryffindor",
-      },
-      {
-        choice: "Analyze the door and its potential mysteries.",
-        category: "Ravenclaw",
-      },
-      {
-        choice: "Find a way to pick the lock and uncover the secrets inside.",
-        category: "Slytherin",
-      },
-      {
-        choice: "Kick down the door and fearlessly explore the forbidden area.",
-        category: "Hufflepuff",
-      },
-    ],
-  },
-  {
-    text: "You witness an argument between two friends. What do you do?",
-    choices: [
-      {
-        choice: "Mediate and try to find a peaceful resolution.",
-        category: "Gryffindor",
-      },
-      {
-        choice: "Listen to both sides and provide logical advice.",
-        category: "Ravenclaw",
-      },
-      {
-        choice: "Use the situation to your advantage and manipulate them.",
-        category: "Slytherin",
-      },
-      {
-        choice: "Stand up for one friend and confront the other if necessary.",
-        category: "Hufflepuff",
-      },
-    ],
-  },
-  {
-    text: "A classmate accuses you of stealing their belongings. What is your response?",
-    choices: [
-      {
-        choice: "Apologize and help them search for the missing items.",
-        category: "Gryffindor",
-      },
-      {
-        choice: "Offer to investigate and gather evidence to clear your name.",
-        category: "Ravenclaw",
-      },
-      {
-        choice: "Defend yourself and try to turn the accusation back on them.",
-        category: "Slytherin",
-      },
-      {
-        choice: "Prove your innocence through bravery and taking risks.",
-        category: "Hufflepuff",
-      },
-    ],
-  },
-  {
-    text: "You have the opportunity to learn a new spell that is known for its power. What do you do?",
-    choices: [
-      {
-        choice: "Learn the spell and use it responsibly to help others",
-        category: "Gryffindor",
-      },
-      {
-        choice: "Study the spell extensively to fully understand its potential",
-        category: "Ravenclaw",
-      },
-      {
-        choice: "Master the spell and use it to gain influence and control",
-        category: "Slytherin",
-      },
-      {
-        choice: "Question the ethical implications before deciding to learn it",
-        category: "Hufflepuff",
-      },
-    ],
-  },
-  {
-    text: "You find a lost object in the hallway. What do you do?",
-    choices: [
-      {
-        choice: "Try to find the owner and return the object immediately",
-        category: "Gryffindor",
-      },
-      {
-        choice: "Examine the object and try to deduce its origin and purpose",
-        category: "Ravenclaw",
-      },
-      {
-        choice: "Keep the object for yourself and your friends for future use.",
-        category: "Slytherin",
-      },
-      {
-        choice: "Turn the object over to a professor or authority figure",
-        category: "Hufflepuff",
-      },
-    ],
-  },
-  {
-    text: "You are presented with a riddle that promises great rewards if solved. What is your approach?",
-    choices: [
-      {
-        choice: "Collaborate with others and work together to solve the riddle",
+        choice:
+          "While we may come from different places and speak in different tongues, our hearts beat as one.",
         category: "Gryffindor",
       },
       {
         choice:
-          "Analyze the riddle's structure and research relevant information",
+          "Differences of habit and language are nothing at all if our aims are identical and our hearts are open.",
         category: "Ravenclaw",
       },
       {
-        choice: "Cheat or transform the situation to obtain the rewards",
+        choice:
+          "The truth: It is a beautiful and terrible thing, and should therefore be treated with caution.",
         category: "Slytherin",
       },
       {
         choice:
-          "Face the challenge head-on and use intuition to solve the riddle",
+          "Youth can not know how age thinks and feels, but old men are guilty if they forget what it is to be young",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "You discover a secret passage within the castle. What do you do?",
+    text: "If you could attend a magical event, which one would you choose?",
     choices: [
       {
-        choice: "Share the discovery with friends and explore it together",
+        choice: "Wizarding World Quidditch Cup",
         category: "Gryffindor",
       },
       {
-        choice: "Document the passage's location and investigate its history",
+        choice: "Creation of new and difficult spells",
         category: "Ravenclaw",
       },
       {
-        choice:
-          "Exploit the passage and explore future advantages it could bring you or your friends.",
+        choice: "Triwizard Tournament",
         category: "Slytherin",
       },
       {
-        choice: "Bravely venture into the unknown to uncover its secrets",
+        choice: "Magical Creatures Convention",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "Your friend is being bullied by a group of students. How do you respond?",
+    text: "Which magical spell do you find most intriguing?",
     choices: [
       {
-        choice: "Stand up for your friend and confront the bullies",
+        choice: "Expecto Patronum",
         category: "Gryffindor",
       },
       {
-        choice:
-          "Analyze the situation and strategize the best course of action",
+        choice: "Petrificus Totalus",
         category: "Ravenclaw",
       },
       {
-        choice:
-          "Use the opportunity and ask your friend a favor from him for the help you provided.",
+        choice: "Impero",
         category: "Slytherin",
       },
       {
-        choice:
-          "Support your friend emotionally and seek help from authorities",
+        choice: "Expluso",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "You have the chance to attend a lecture on a magical topic of your choice. What do you choose?",
-    choices: [
-      { choice: "Defensive spells and techniques", category: "Gryffindor" },
-      {
-        choice: "Advanced potion-making and ingredients",
-        category: "Ravenclaw",
-      },
-      { choice: "Dark arts and forbidden curses", category: "Slytherin" },
-      { choice: "Magical creatures and their care", category: "Hufflepuff" },
-    ],
-  },
-  {
-    text: "You stumble upon a hidden artifact that is rumored to grant great power. What do you do?",
+    text: "If you could have one of the Deathly Hallows, which one would you choose?",
     choices: [
       {
-        choice: "Leave the artifact untouched and keep its existence a secret",
+        choice: "Elder Wand",
         category: "Gryffindor",
       },
       {
-        choice:
-          "Research the artifact's history and potential risks before deciding",
+        choice: "Resurrection Stone",
         category: "Ravenclaw",
       },
       {
-        choice: "Claim the artifact and wield its power",
+        choice: "Invisibility Cloak",
         category: "Slytherin",
       },
       {
-        choice: "Destroy or dispose of the artifact to prevent misuse",
+        choice: "Ignotus Peverell's Ring",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "A fellow student challenges your beliefs and values. How do you respond?",
+    text: "Which magical location would you most like to visit?",
     choices: [
       {
-        choice:
-          "Engage in a respectful debate to understand different perspectives",
+        choice: "Hogwarts School of Witchcraft and Wizardry",
         category: "Gryffindor",
       },
       {
-        choice: "Manipulate or deceive them to protect your beliefs",
+        choice: "The Library at Hogwarts",
         category: "Ravenclaw",
       },
       {
-        choice: "Stand firm in your convictions and defend your values",
+        choice: "The Dark Forest",
         category: "Slytherin",
       },
       {
-        choice: "Seek common ground and find a compromise or understanding",
+        choice: "Hagrid's Hut",
         category: "Hufflepuff",
       },
     ],
   },
   {
-    text: "You witness an injustice happening within the Hogwarts community. What do you do?",
+    text: "If you could befriend any character from the Harry Potter series, who would it be?",
     choices: [
       {
-        choice: "Take immediate action to rectify the injustice",
+        choice: "Neville Longbottom",
         category: "Gryffindor",
       },
       {
-        choice: "Gather evidence and formulate a plan to expose the injustice",
+        choice: "Cho Chang",
         category: "Ravenclaw",
       },
       {
-        choice:
-          "Exploit the situation to your advantage or join the oppressors",
+        choice: "Gregory Goyle",
         category: "Slytherin",
       },
       {
-        choice:
-          "Support the affected individual and rally others for a fair resolution",
+        choice: "Cedric Diggory",
+        category: "Hufflepuff",
+      },
+    ],
+  },
+  {
+    text: "Which magical creature do you find most fascinating?",
+    choices: [
+      {
+        choice: "Dragons",
+        category: "Gryffindor",
+      },
+      {
+        choice: "Thestrals",
+        category: "Ravenclaw",
+      },
+      {
+        choice: "Basilisks",
+        category: "Slytherin",
+      },
+      {
+        choice: "Hippogriffs",
+        category: "Hufflepuff",
+      },
+    ],
+  },
+  {
+    text: "Which Hogwarts professor do you admire the most?",
+    choices: [
+      {
+        choice: "Professor Burbage",
+        category: "Gryffindor",
+      },
+      {
+        choice: "Professor Umbridge",
+        category: "Ravenclaw",
+      },
+      {
+        choice: "Professor Slughorn",
+        category: "Slytherin",
+      },
+      {
+        choice: "Professor Cuthbert",
+        category: "Hufflepuff",
+      },
+    ],
+  },
+  {
+    text: "If you could own a magical artifact, which one would it be?",
+    choices: [
+      {
+        choice: "Sorting Hat",
+        category: "Gryffindor",
+      },
+      {
+        choice: "The Marauder's Map",
+        category: "Ravenclaw",
+      },
+      {
+        choice: "The Mirror of Erised",
+        category: "Slytherin",
+      },
+      {
+        choice: "Alastor Moody's eye",
+        category: "Hufflepuff",
+      },
+    ],
+  },
+  {
+    text: "Which Quidditch position would you prefer to play?",
+    choices: [
+      {
+        choice: "Seeker",
+        category: "Gryffindor",
+      },
+      {
+        choice: "Chaser",
+        category: "Ravenclaw",
+      },
+      {
+        choice: "Beater",
+        category: "Slytherin",
+      },
+      {
+        choice: "Keeper",
+        category: "Hufflepuff",
+      },
+    ],
+  },
+  {
+    text: "If you could learn any form of magical transportation, which one would you choose?",
+    choices: [
+      {
+        choice: "Apparition",
+        category: "Gryffindor",
+      },
+      {
+        choice: "Portkey",
+        category: "Ravenclaw",
+      },
+      {
+        choice: "Floo Powder",
+        category: "Slytherin",
+      },
+      {
+        choice: "Broomstick",
         category: "Hufflepuff",
       },
     ],
